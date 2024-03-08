bba53101bd7cac506f10e01c5996e7211396b2da
YaroslavShh <yaroslav.shamov@list.ru>
2024-03-08 04:03:47 +0300

    ver 1.2 (change z-index)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const openPopupBtn = document.getElementById("main__btn");
-const openPopupBtnisMenu = document.getElementById("menuListBtn");
+const openPopupBtnIsMenu = document.getElementById("menuListBtn");
 const popupCloseBtn = document.getElementById("popupClose");
 const popup = document.getElementById("popup");
 const menuCloseBtn = document.getElementById("menuClose");
@@ -9,7 +9,7 @@ const menuOpenBtn = document.getElementById("headerMenuBurger");
 const universalFunc = (block) => block.style.top === "-100%" ? block.style.top = "0" : block.style.top = "-100%";
 
 openPopupBtn.addEventListener("click", () => universalFunc(popup));
-openPopupBtnisMenu.addEventListener("click", () => universalFunc(popup));
+openPopupBtnIsMenu.addEventListener("click", () => universalFunc(popup));
 popupCloseBtn.addEventListener("click",() =>  universalFunc(popup))
 menuOpenBtn.addEventListener("click", () => universalFunc(headerMenu))
 menuCloseBtn.addEventListener("click", () => universalFunc(headerMenu))
