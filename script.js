ea0af1f305bb2ae32d2fdebd777f7b32e09c9906
YaroslavShh <yaroslav.shamov@list.ru>
2024-03-08 05:54:09 +0300

    ver 1.4 (form validate)

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const popup = document.getElementById("popup");
 const menuCloseBtn = document.getElementById("menuClose");
 const headerMenu = document.getElementById("headerMenu");
 const menuOpenBtn = document.getElementById("headerMenuBurger");
+const formSubmit = document.getElementById("formSubmit");
+const inputText = document.getElementById("inputText");
+const inputCheckbox = document.getElementById("inputCheckbox");
+const formPrivacy = document.getElementById("formPrivacy");
 
 const universalFunc = (block) => block.style.top === "-300%" ? block.style.top = "0" : block.style.top = "-300%";
 
@@ -13,3 +17,22 @@ openPopupBtnIsMenu.addEventListener("click", () => universalFunc(popup));
 popupCloseBtn.addEventListener("click",() =>  universalFunc(popup))
 menuOpenBtn.addEventListener("click", () => universalFunc(headerMenu))
 menuCloseBtn.addEventListener("click", () => universalFunc(headerMenu))
+formSubmit.addEventListener("click", (e) => {
+    e.preventDefault()
+    if(inputText.value.trim() !== "" && inputCheckbox.checked) {
+        alert("The form has been successfully submitted");
+        inputText.value = "";
+        inputCheckbox.checked = false;
+        inputText.classList.remove("red")
+        formPrivacy.style.color = null;
+    } else if (inputText.value.trim() !== "" && inputCheckbox.checked === false) {
+        inputText.classList.remove("red")
+        formPrivacy.style.color = "red";
+    } else if (inputText.value.trim() === "" && inputCheckbox.checked === true){
+        inputText.classList.add("red")
+        formPrivacy.style.color = null;
+    } else {
+        inputText.classList.add("red");
+        formPrivacy.style.color = "red";
+    }
+})
